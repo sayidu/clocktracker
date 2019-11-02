0140e50e33ef3e6ab155094731e2b5da4554a2a5
Shalom Ayidu <you@example.com>
2019-11-03 00:18:20 +0100

    Leverage Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def humanize(week_day)
+    TimeLog.constants.find { |k| TimeLog.const_get(k) == week_day }.to_s.capitalize
+  end
+
+  def humanize(check_in_purpose)
+    TimeLog.constants.find { |k| TimeLog.const_get(k) == check_in_purpose }.to_s.capitalize
+  end 
 end
